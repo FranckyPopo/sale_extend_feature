e801fd554de3f200501f15b775ad1da89c09cc18
Afri <afrifranck@gmail.com>
2024-07-12 15:17:00 +0000

    Le model Car  et la vue car on été étendu via ce module

--- __manifest__.py
+++ __manifest__.py
@@ -14,16 +14,16 @@
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/15.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
-    'category': 'sale',
+    'category': 'Uncategorized',
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base',],
+    'depends': ['base', 'car_management'],
 
     # always loaded
     'data': [
         # 'security/ir.model.access.csv',
-        'views/views.xml',
+        'views/car_management_views.xml',
         'views/templates.xml',
     ],
     # only loaded in demonstration mode
--- models/car_management_car.py
+++ models/car_management_car.py
@@ -3,10 +3,6 @@ from odoo import models, fields, api
 
 class InheritedCar(models.Model):
     _inherit = "car_management.car"
-
     test_field = fields.Char(string='Ceci est un champs ajouté par un autres module')
 
-class Test(models.Model):
-    _name = "sale_extend_feature.test"
 
-    field_test = fields.Char(string='Ceci est un test')
